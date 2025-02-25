109e5ee91622105fbc470d542dd380f0e328e266
beejh02 <joon0448@naver.com>
2025-02-25 23:00:28 +0900

    [feat] axios : axios에서 헤더의 JWT를 불러오고 저장하는 기능 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,10 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8080",
 });
 
-export default api;
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt") || sessionStorage.getItem("jwt");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
